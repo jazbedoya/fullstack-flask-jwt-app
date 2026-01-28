df0f0c181dfd0a0f3593d0f19d355c290e1a994a
Jazmin <jbedoya628@gmail.com>
2026-01-28 00:26:43 -0300

    ya

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-dev")
